86d44549dec610f880d9a8a9ee97bc9a6b8b1784
N-Andronopoulos <nikolas.andronopoulos@gmail.com>
2014-12-26 21:56:32 +0200

    Introduced: loggers aka log4js.

--- index.js
+++ index.js
@@ -4,9 +4,21 @@
  * @version 0.1.0
  */
 
+//Configs
 var config = require("./config/config.json");
 var loginInfo = require('./config/real-info.json');
 var moveMailService = require("./lib/findProcessMoveMail.js");
 
-moveMailService.init(loginInfo, config);
+//Logger stuff.
+var loggerConfig = require("./config/loggerConfig");
+var log4js = require('log4js');
+log4js.configure(loggerConfig);
 
+//Main (index.js) logger.
+var logger = log4js.getLogger("Main program.");
+
+logger.info("System starting...");
+
+moveMailService.init(loginInfo, config, log4js.getLogger("Mail move service."));
+
+//TODO File Logger doesn't write anything to it!...
