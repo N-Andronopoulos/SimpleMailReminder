a4618bde01d7c8c330a3640527efdb0a40533559
N-Andronopoulos <nikolas.andronopoulos@gmail.com>
2014-12-26 18:25:57 +0200

    Rearranged the projects structure.

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
  * @version 0.1.0
  */
 
-var config = require("./config.json");
-var moveMailService = require("./findProcessMoveMail.js");
+var config = require("./config/config.json");
+var loginInfo = require('./config/real-info.json');
+var moveMailService = require("./lib/findProcessMoveMail.js");
 
-moveMailService.init(config);
+moveMailService.init(loginInfo, config);
 
