1418e4ab4682ce58ca9bdf73bdef71b0a0e31c09
Nikolas Andronopoulos <nikolas.andronopoulos@gmail.com>
2014-12-28 16:29:41 +0200

    Added the mailparsing lib to the sending service lib.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var config = require("./config/config.json");
 var loginInfo = require('./config/real-info.json');
 var moveMailService = require("./lib/findProcessMoveMail.js");
 var cleanMailBox = require("./lib/cleanMailBox.js");
+var sendMailService = require('./lib/mailSender.js');
 
 
 //Logger stuff.
@@ -21,8 +22,14 @@ var logger = log4js.getLogger("Main program.");
 
 logger.info("System starting...");
 
-moveMailService.init(loginInfo, config, log4js.getLogger("Mail move service."));
+//moveMailService.init(loginInfo, config, log4js.getLogger("Mail move service."));
 
 //cleanMailBox.init(loginInfo, config, log4js.getLogger("MailBox clean service."));
 
-cleanMailBox.init(loginInfo, config, log4js.getLogger("MailBox clean service."));
+//cleanMailBox.init(loginInfo, config, log4js.getLogger("MailBox clean service."));
+
+var fs = require('fs');
+var path = require('path');
+var mail = fs.readFileSync(path.join(__dirname,'tmp','data-0.txt'));
+
+sendMailService.sendMail(loginInfo,mail,log4js.getLogger("Mail Sending Service."));
