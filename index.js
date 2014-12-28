07831f2e1d9b62babf1e2eab0bfd7a2f509f46c3
Nikolas Andronopoulos <nikolas.andronopoulos@gmail.com>
2014-12-28 10:59:58 +0200

    Spring cleaning.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var config = require("./config/config.json");
 var loginInfo = require('./config/real-info.json');
 var moveMailService = require("./lib/findProcessMoveMail.js");
 var cleanMailBox = require("./lib/cleanMailBox.js");
-var checkReminds = require("./lib/checkReminds.js");
+
 
 //Logger stuff.
 var loggerConfig = require("./config/loggerConfig");
@@ -21,8 +21,8 @@ var logger = log4js.getLogger("Main program.");
 
 logger.info("System starting...");
 
-//moveMailService.init(loginInfo, config, log4js.getLogger("Mail move service."));
+moveMailService.init(loginInfo, config, log4js.getLogger("Mail move service."));
 
 //cleanMailBox.init(loginInfo, config, log4js.getLogger("MailBox clean service."));
 
-checkReminds.init(loginInfo, config, log4js.getLogger("MailBox checkReminds service."));
+cleanMailBox.init(loginInfo, config, log4js.getLogger("MailBox clean service."));
