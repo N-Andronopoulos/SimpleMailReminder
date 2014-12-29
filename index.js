bfe3b8513cd6f2e296a55bdbfac31b5f2ff6c761
Nikolas Andronopoulos <nikolas.andronopoulos@gmail.com>
2014-12-29 19:59:53 +0200

    Made exports work without the init(), striped package.json of unsed modules, fixed bug in setup.js and fix test to run nothing successfuly.

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ var config = require("./config/config.json");
 var loginInfo = require('./config/real-info.json');
 var moveMailService = require("./lib/findProcessMoveMail.js");
 var cleanMailBox = require("./lib/cleanMailBox.js");
-var sendMailService = require('./lib/mailSender.js');
-
+var checkRemindService = require('./lib/checkReminds.js');
 
 //Logger stuff.
 var loggerConfig = require("./config/loggerConfig");
@@ -22,14 +21,9 @@ var logger = log4js.getLogger("Main program.");
 
 logger.info("System starting...");
 
-//moveMailService.init(loginInfo, config, log4js.getLogger("Mail move service."));
+moveMailService(loginInfo, config, log4js.getLogger("[Mail move service]"));
 
-//cleanMailBox.init(loginInfo, config, log4js.getLogger("MailBox clean service."));
+cleanMailBox(loginInfo, config, log4js.getLogger("[MailBox clean service]"));
 
-//cleanMailBox.init(loginInfo, config, log4js.getLogger("MailBox clean service."));
+checkRemindService(loginInfo, config, log4js.getLogger("[Mail check service]"));
 
-var fs = require('fs');
-var path = require('path');
-var mail = fs.readFileSync(path.join(__dirname,'tmp','data-0.txt'));
-
-sendMailService.sendMail(loginInfo,mail,log4js.getLogger("Mail Sending Service."));
--- test/test.js
+++ test/test.js
@@ -2,5 +2,5 @@
  * Created by Nikolas on 24/11/2014.
  */
 
-console.log("No tests yet");
-process.exit(1);
+console.log("No tests yet :(");
+process.exit(0);
